0fbaba1b76383638133a18ae17e3f869f81e49be
Irina Klepova <ivklepova23@gmail.com>
2023-09-13 21:46:17 +0400

    change test_arrs.py (z.4)

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -13,7 +13,3 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
         self.assertEqual(arrs.my_slice([], ), [])
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], end=3), [1, 2, 3])
-
-
-if __name__ == '__main__':
-    unittest.main()
